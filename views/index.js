16e07af26ff861b454716d783d035d24c1aac995
Scott <struaxw@gmail.com>
2016-04-17 10:21:45 -0700

    j

---
+++ views/index.js
@@ -0,0 +1,34 @@
+ <script src="https://maps.googleapis.com/maps/api/js?callback=initMap"
+        async defer></script>
+ <script>
+    function initMap() {
+  var myLatLng = {lat: 33.1151217, lng: -117.0802894};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Dels Barber Shop'
+  });
+}
+      
+<<<<<<< HEAD
+    </script>
+
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+</script>
+
+=======
+</script>
+>>>>>>> origin/master
