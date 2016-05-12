0e02899f22bf2a514190493f02ec521604f0c9ee
Scott <struaxw@gmail.com>
2016-05-12 16:32:50 -0700

    equal column height

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,23 @@
+
+ var $col_1 = $("#col_1"); 
+ var $col_2 = $("#col_2");
+$(document).ready(function{
+	var col1 = $col_1;
+    var col2 = $col_2;
+    var height = 0;
+    if(col1.height() >= col2.height()){
+    	height=col1.height();
+    	alert(height);
+    }else{
+    	height=col2.height();
+    	alert(height);
+
+    }
+
+
+});
+
+
 var express = require('express');
 var router = express.Router();
 
