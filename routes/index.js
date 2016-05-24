6fd8ceccdde0c9bc8e38f9877368c34730f7c266
Luke Chatton <chattonluke@gmail.com>
2016-05-23 19:52:06 -0700

    add title

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Del's Barber Shop" });
 });
 
 router.get('/history', function(req, res, next) {
